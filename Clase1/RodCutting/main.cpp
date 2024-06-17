0f582e5d3525e1d17a41bc9edba0bcb4bcf31d73
Gerardo Rosetti <gasayago@gmail.com>
2024-06-17 18:02:39 -0400

    final

--- Clase1/RodCutting/main.cpp
+++ Clase1/RodCutting/main.cpp
@@ -2,29 +2,29 @@
 
 int main()
 {
-    vector<ull> prices_table = {0, 1, 5, 8, 9};
+    std::vector<ull> prices_table = {0, 1, 5, 8, 9};
     //vector<ull> prices_table = {0, 1, 5, 8, 9, 10, 17};
     //vector<ull> prices_table = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     //vector<ull> prices_table = {0, 1, 2, 4, 6, 9, 10, 16, 18};
     ull n = prices_table.size() - 1;
 
     // Crónica para el algoritmo recursivo básico
-    auto start_basic = chrono::steady_clock::now();
+    auto start_basic = std::chrono::steady_clock::now();
     ull result_basic = rod_cutting(prices_table, n);
-    auto end_basic = chrono::steady_clock::now();
-    cout << "Resultado con Algoritmo Recursivo Básico: " << result_basic << endl;
-    cout << "Tiempo con Algoritmo Recursivo Básico: "
-         << chrono::duration_cast<chrono::nanoseconds>(end_basic - start_basic).count()
+    auto end_basic = std::chrono::steady_clock::now();
+    std::cout << "Resultado con Algoritmo Recursivo Básico: " << result_basic << std::endl;
+    std::cout << "Tiempo con Algoritmo Recursivo Básico: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end_basic - start_basic).count()
          << " nanosegundos\n";
 
     // Crónica para el algoritmo recursivo con memoización
-    vector<ull> mem(n + 1, 0);
-    auto start_memo = chrono::steady_clock::now();
+    std::vector<ull> mem(n + 1, 0);
+    auto start_memo = std::chrono::steady_clock::now();
     ull result_memo = rod_cutting_memo(prices_table, mem, n);
-    auto end_memo = chrono::steady_clock::now();
-    cout << "Resultado con Algoritmo Recursivo con Memoización: " << result_memo << endl;
-    cout << "Tiempo con Algoritmo Recursivo con Memoización: "
-         << chrono::duration_cast<chrono::nanoseconds>(end_memo - start_memo).count()
+    auto end_memo = std::chrono::steady_clock::now();
+    std::cout << "Resultado con Algoritmo Recursivo con Memoización: " << result_memo << std::endl;
+    std::cout << "Tiempo con Algoritmo Recursivo con Memoización: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end_memo - start_memo).count()
          << " nanosegundos\n";
 
     return 0;
--- Clase1/RodCutting/rodCutting.hpp
+++ Clase1/RodCutting/rodCutting.hpp
@@ -3,11 +3,9 @@
 #include <chrono>
 #include <unordered_map>
 
-using namespace std;
-using namespace std::chrono;
 using ull = unsigned long long;
 
-ull rod_cutting(const vector<ull>& prices_table, ull n)
+ull rod_cutting(const std::vector<ull>& prices_table, ull n)
 {
     if(n <= 0)
     {
@@ -18,13 +16,13 @@ ull rod_cutting(const vector<ull>& prices_table, ull n)
 
     for (ull i = 1; i <= n; ++i)
     {
-        m = max(m, prices_table[i] + rod_cutting(prices_table, n - i));
+        m = std::max(m, prices_table[i] + rod_cutting(prices_table, n - i));
     }
 
     return m;
 }
 
-ull rod_cutting_memo(const vector<ull>& prices_table, vector<ull>& mem, ull n)
+ull rod_cutting_memo(const std::vector<ull>& prices_table, std::vector<ull>& mem, ull n)
 {
     if (mem[n] > 0)
     {
@@ -37,7 +35,7 @@ ull rod_cutting_memo(const vector<ull>& prices_table, vector<ull>& mem, ull n)
     {
         for (ull i = 1; i <= n; ++i)
         {
-            m = max(m, prices_table[i] + rod_cutting_memo(prices_table, mem, n - i));
+            m = std::max(m, prices_table[i] + rod_cutting_memo(prices_table, mem, n - i));
         }
     }
 
