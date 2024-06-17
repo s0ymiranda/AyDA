e5879fa78077b244d2bffa0e5422ddc72921c7ae
Gerardo Rosetti <gasayago@gmail.com>
2024-06-17 17:57:02 -0400

    better

--- Clase1/RodCutting/main.cpp
+++ Clase1/RodCutting/main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    //vector<ull> prices_table = {0, 1, 5, 8, 9};
+    vector<ull> prices_table = {0, 1, 5, 8, 9};
     //vector<ull> prices_table = {0, 1, 5, 8, 9, 10, 17};
     //vector<ull> prices_table = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    vector<ull> prices_table = {0, 1 ,2 , 4, 6, 9, 10, 16, 18};
+    //vector<ull> prices_table = {0, 1, 2, 4, 6, 9, 10, 16, 18};
     ull n = prices_table.size() - 1;
 
     // Crónica para el algoritmo recursivo básico
